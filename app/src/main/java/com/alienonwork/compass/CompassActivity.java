10b6b8ddfcf422fe1ddc657ee60b45146b777b78
Costa <weuller.costa@capgemini.com>
2019-04-21 09:04:43 -0300

    First commit, with compass custom view

---
+++ app/src/main/java/com/alienonwork/compass/CompassActivity.java
@@ -0,0 +1,13 @@
+package com.alienonwork.compass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+    }
+}
